31ea39a0c1b21c4856abb3595734a8ec5ce35909
Diki Armanda <diki.armada5@gmail.com>
2024-01-26 15:47:35 +0700

    2/366

---
+++ 2-reverseSeq.js
@@ -0,0 +1,18 @@
+// Coding Challenge 2024
+// 2/366
+// https://www.codewars.com/kata/5a00e05cc374cb34d100000d
+
+// const reverseSeq = n => {
+//   const result = []
+//   for (let i = n; i >= 1; i--) {
+//     result.push(i)
+//   }
+//   return result;
+// }
+
+const reverseSeq = n => {
+  // return Array(n).fill().map((e, i) => i+1).reverse()
+  return [...Array(n)].map((e, i) => n-i)
+}
+
+console.log(reverseSeq(5));
