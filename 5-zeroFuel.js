77df8edfab3d38bfd9406a8d6d5d24d129ddacad
Diki Armanda <diki.armada5@gmail.com>
2024-01-28 14:53:06 +0700

    5/366

---
+++ 5-zeroFuel.js
@@ -0,0 +1,11 @@
+// Coding Challenge 2024
+// 5/366
+// https://www.codewars.com/kata/5861d28f124b35723e00005e
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//   return (mpg * fuelLeft >= distanceToPump)
+// }
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => mpg * fuelLeft >= distanceToPump
+
+console.log(zeroFuel(100, 50, 1));
