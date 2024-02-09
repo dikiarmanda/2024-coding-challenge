52d61e234d87f88eee29effe1bd1d089578b35c8
Diki Armanda <diki.armada5@gmail.com>
2024-02-09 21:00:00 +0700

    17/366

---
+++ 17-areaOrPerimeter.js
@@ -0,0 +1,15 @@
+// Coding Challenge 2024
+// 17/366
+// https://www.codewars.com/kata/5ab6538b379d20ad880000ab
+
+// const areaOrPerimeter = function (l, w) {
+//   if (l == w) {
+//     return l * w
+//   } else {
+//     return (l + w) * 2
+//   }
+// }
+
+const areaOrPerimeter = (l, w) => (l == w) ? l * w : (l + w) * 2
+
+console.log(areaOrPerimeter(6, 10))
