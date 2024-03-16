dbd069267a1f80ed3ffa516626439337da208458
Diki Armanda <diki.armada5@gmail.com>
2024-03-17 04:33:04 +0700

    32/366

---
+++ 32-cockroachSpeed.js
@@ -0,0 +1,12 @@
+// Coding Challenge 2024
+// 32/366
+// https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6
+
+// function cockroachSpeed(s) {
+//   return Math.floor(s * 100000 / 3600)
+// }
+
+// const cockroachSpeed = (s) => Math.floor(s * 100000 / 3600)
+const cockroachSpeed = (s) => Math.floor(s / 0.036)
+
+console.log(cockroachSpeed(1.08))
