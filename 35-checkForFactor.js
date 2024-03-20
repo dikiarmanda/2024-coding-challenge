af8e6bc6440031d8d3afdeabab4fe17440a9ff7a
Diki Armanda <diki.armada5@gmail.com>
2024-03-20 19:29:58 +0700

    35/366

---
+++ 35-checkForFactor.js
@@ -0,0 +1,11 @@
+// Coding Challenge 2024
+// 35/366
+// https://www.codewars.com/kata/55cbc3586671f6aa070000fb
+
+// function checkForFactor(base, factor) {
+//   return base % factor === 0
+// }
+
+const checkForFactor = (base, factor) => base % factor === 0
+
+console.log(checkForFactor(24617, 3));
