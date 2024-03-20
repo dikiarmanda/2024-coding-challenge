f1f45d89292c5b918e08e7c5c7d68321d68a222b
Diki Armanda <diki.armada5@gmail.com>
2024-03-20 19:30:04 +0700

    36/366

---
+++ 36-hoopCount.js
@@ -0,0 +1,14 @@
+// Coding Challenge 2024
+// 36/366
+// https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145
+
+// function hoopCount(n) {
+//   if (n >= 10) {
+//     return "Great, now move on to tricks";
+//   }
+//   return "Keep at it until you get it";
+// }
+
+const hoopCount = (n) => (n >= 10) ? "Great, now move on to tricks" : "Keep at it until you get it"
+
+console.log(hoopCount(30));
